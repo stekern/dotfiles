597f44019101c8da6da2da7ab5bc51865ccd4bfe
stekern <stekern@users.noreply.github.com>
2018-08-17 15:18:49 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -6,9 +6,13 @@
 # Distributed under terms of the MIT license.
 #
 
+git clone https://github.com/stekern/dotfiles && cd dotfiles
+
+DEFAULT_BASE16_THEME="base16-snazzy"
+
 # Symlinking
 declare -A symlinks=(
-    ["vimrc"]=~/.config/nvim/init.vim
+    ["./vim/vimrc"]=~/.config/nvim/init.vim
     ["gitconfig"]=~/.gitconfig
     ["tmux.conf"]=~/.tmux.conf
     ["zshrc"]=~/.zshrc
@@ -16,44 +20,51 @@ declare -A symlinks=(
 
 for filename in "${!symlinks[@]}"; do
     file=$(realpath "$filename")
-    [ -f "$file" ] && echo ln -s "$file" "${symlinks[$filename]}"
+    echo "ln -s $file ${symlinks[$filename]}"
+    # [ -f "$file" ] && echo ln -s "$file" "${symlinks[$filename]}"
 done
 exit
 
-# Installing oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Install oh-my-zsh
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Installing neovim
+# Install neovim, tmux and python
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt install neovim
-sudo apt install python-dev python-pip python3-dev python3-pip
-ln -s /usr/bin/nvim /usr/bin/vim ### TO do: Handle vim already installed
-
-### To do: Add custom vim-templates etc. to repo
-
-# Installing tmux
 sudo apt install tmux
+sudo apt install python-dev python-pip python3-dev python3-pip
 
-# Installing powerline fonts
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
+# TODO: Set up pyenv, neovim-python, ...
 
-# Installing base16-shell
-if [ ! -d ~/.config/base16-shell ]; then
-    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+# Symlink or alias nvim to vim
+if [ -f /usr/bin/vim ]; then
+    alias vim="nvim"
+else
+    ln -s /usr/bin/nvim /usr/bin/vim
 fi
 
-# Installing base-16-gnome-terminal
+
+# TODO: Add custom vim-templates etc. to repo
+
+
+# Install powerline fonts
+git clone https://github.com/powerline/fonts.git --depth=1 && cd fonts && ./install.sh && cd ..  && rm -rf fonts
+
+# Install base16-shell and set theme
+if [ ! -d ~/.config/base16-shell ]; then
+    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+    (source ~/.zshrc)
+    ~/.config/base16-shell/scripts/$DEFAULT_BASE16_THEME.sh
+fi
+
+# Install base-16-gnome-terminal
 if [ ! -d ~/.config/base16-gnome-terminal ]; then
     git clone https://github.com/chriskempson/base16-gnome-terminal.git ~/.config/base16-gnome-terminal
 
     DFCONFDIR='/org/gnome/terminal/legacy/profiles:'
     old_profiles=($(dconf list /org/gnome/terminal/legacy/profiles:/ | egrep '^:' | sed 's/[:\/]//g'))
-    (source ~/.config/base16-gnome-terminal/base16-default.light.sh)
+    (source ~/.config/base16-gnome-terminal/base16-brewer.dark.sh)
     new_profiles=($(dconf list /org/gnome/terminal/legacy/profiles:/ | egrep '^:' | sed 's/[:\/]//g'))
     added_profiles=$((${#new_profiles[@]}-${#old_profiles[@]}))
 
