a24c20e07e9c15a40b9756572c5e7b80c3803162
stekern <stekern@users.noreply.github.com>
2026-06-01 09:22:14 +0200

    keep zoom when jumping to prev tmux pane

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -8,13 +8,12 @@ return {
     init = function()
       vim.g.tmux_navigator_disable_when_zoomed = 1
     end,
-    cmd = { 'TmuxNavigateLeft', 'TmuxNavigateDown', 'TmuxNavigateUp', 'TmuxNavigateRight', 'TmuxNavigatePrevious' },
+    cmd = { 'TmuxNavigateLeft', 'TmuxNavigateDown', 'TmuxNavigateUp', 'TmuxNavigateRight' },
     keys = {
       { '<M-h>', '<cmd>TmuxNavigateLeft<cr>' },
       { '<M-j>', '<cmd>TmuxNavigateDown<cr>' },
       { '<M-k>', '<cmd>TmuxNavigateUp<cr>' },
       { '<M-l>', '<cmd>TmuxNavigateRight<cr>' },
-      { '<M-p>', '<cmd>TmuxNavigatePrevious<cr>' },
     },
   },
   {
